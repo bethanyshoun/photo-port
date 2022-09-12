cedceacc69515901eed5454b06987ab0793421e2
bethanyshoun <bethanyshoun@gmail.com>
2022-09-11 21:22:42 -0400

    about feature

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
+import Nav from './components/Nav';
 import About from './components/About';
-//import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div>
-      <main>
-        <About></About>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Nav></Nav>
+            <main>
+                <About></About>
+            </main>
+        </div>
+    );
 }
-
 
 export default App;
